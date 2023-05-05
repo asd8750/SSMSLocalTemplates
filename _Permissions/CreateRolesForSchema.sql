8e6a981dba3307d64ca330bd1eb57f667462b33c
FredL <Flaforest@laforest.biz>
2023-05-05 08:47:34 -0400

    Correct the grant permissons and add ReadEx

--- _Permissions/CreateRolesForSchema.sql
+++ _Permissions/CreateRolesForSchema.sql
@@ -1,5 +1,5 @@
 
-DECLARE	@schemaName		VARCHAR(128) = 'IRA';
+DECLARE	@schemaName		VARCHAR(128) = 'WIP';
 DECLARE	@schemaNameU	VARCHAR(128);
 DECLARE @sqlSetupPerms	VARCHAR(MAX);
 
@@ -7,35 +7,35 @@ SET @schemaName = SCHEMA_NAME(SCHEMA_ID(@schemaName));	-- Get the current case n
 SET @schemaNameU = STUFF(@schemaName,1,1,UPPER(LEFT(@schemaName,1))); -- Upper case the first character for Camel casing
 
 SET @sqlSetupPerms = '
-	USE [' + DB_NAME() + ']
-	IF DATABASE_PRINCIPAL_ID(''roleSch<<SchU>>_Admin'') IS NOT NULL
-		DROP ROLE [roleSch<<SchU>>_Admin];
-	CREATE ROLE [roleSch<<SchU>>_Admin] AUTHORIZATION [dbo];
-	GRANT CREATE TABLE, CREATE PROCEDURE, CREATE FUNCTION, CREATE VIEW TO  [roleSch<<SchU>>_Admin];
-	GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE, ALTER, REFERENCES ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Admin];
-	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Admin];
-
-	IF DATABASE_PRINCIPAL_ID(''roleSch<<SchU>>_Update'') IS NOT NULL
-		DROP ROLE [roleSch<<SchU>>_Update];
-	CREATE ROLE [roleSch<<SchU>>_Update] AUTHORIZATION [dbo];
-	GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Update];
-	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Update];
-
-	IF DATABASE_PRINCIPAL_ID(''roleSch<<SchU>>_ReadEX'') IS NOT NULL
-		DROP ROLE [roleSch<<SchU>>_Reader];
-	CREATE ROLE [roleSch<<SchU>>_Reader] AUTHORIZATION [dbo];
-	GRANT SELECT, EXECUTE ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Reader];
-	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Reader];
-
-	IF DATABASE_PRINCIPAL_ID(''roleSch<<SchU>>_ReadOnly'') IS NOT NULL
-		DROP ROLE [roleSch<<SchU>>_Reader];
-	CREATE ROLE [roleSch<<SchU>>_Reader] AUTHORIZATION [dbo];
-	GRANT SELECT TO [roleSch<<SchU>>_Reader];
-	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch<<SchU>>_Reader];
+	IF DATABASE_PRINCIPAL_ID(''roleSch_<<SchU>>_Admin'') IS NULL 
+		CREATE ROLE [roleSch_<<SchU>>_Admin] AUTHORIZATION [dbo]; 
+	GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE, ALTER, REFERENCES ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Admin]; 
+	GRANT CREATE TABLE     TO  [roleSch_<<SchU>>_Admin]; 
+	GRANT CREATE PROCEDURE TO  [roleSch_<<SchU>>_Admin]; 
+	GRANT CREATE FUNCTION  TO  [roleSch_<<SchU>>_Admin]; 
+	GRANT CREATE VIEW      TO  [roleSch_<<SchU>>_Admin]; 
+	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Admin]; 
+	 
+	IF DATABASE_PRINCIPAL_ID(''roleSch_<<SchU>>_Update'') IS NULL 
+		CREATE ROLE [roleSch_<<SchU>>_Update] AUTHORIZATION [dbo]; 
+	GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Update]; 
+	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Update]; 
+	 
+	IF DATABASE_PRINCIPAL_ID(''roleSch_<<SchU>>_ReadExecute'') IS NULL 
+		CREATE ROLE [roleSch_<<SchU>>_ReadExecute] AUTHORIZATION [dbo]; 
+	GRANT SELECT, EXECUTE ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_ReadExecute]; 
+	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_ReadExecute]; 
+	 
+	IF DATABASE_PRINCIPAL_ID(''roleSch_<<SchU>>_Reader'') IS NULL 
+		CREATE ROLE [roleSch_<<SchU>>_Reader] AUTHORIZATION [dbo]; 
+	GRANT SELECT ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Reader]; 
+	GRANT VIEW DEFINITION ON SCHEMA::[<<Sch>>] TO [roleSch_<<SchU>>_Reader]; 
 ';
 
 SET @sqlSetupPerms = REPLACE(@sqlSetupPerms, '<<Sch>>', @schemaName);
 SET @sqlSetupPerms = REPLACE(@sqlSetupPerms, '<<SchU>>', @schemaNameU);
+-- SET @sqlSetupPerms = REPLACE(@sqlSetupPerms, '', CHAR(13)+CHAR(10));
 
 PRINT @sqlSetupPerms;
-EXECUTE (@sqlSetupPerms);
+SELECT @schemaName, @schemaNameU, @sqlSetupPerms;
+--EXECUTE (@sqlSetupPerms);
